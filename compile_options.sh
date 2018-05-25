1a4dee8bdc58a861340ff47ab137155f6bd018be
Eduardo Balbinot <e.balbinot@surrey.ac.uk>
2018-05-26 00:01:49 +0100

    more info

--- compile_options.sh
+++ compile_options.sh
@@ -10,3 +10,19 @@
 							--enable-gui=gtk2 \
 							--enable-cscope \
 							--prefix=/usr/local
+
+If you want to be able to easily uninstall vim use checkinstall.
+
+sudo apt install checkinstall
+cd ~/vim
+sudo checkinstall
+Otherwise, you can use make to install.
+
+cd ~/vim
+sudo make install
+Set vim as your default editor with update-alternatives.
+
+sudo update-alternatives --install /usr/bin/editor editor /usr/local/bin/vim 1
+sudo update-alternatives --set editor /usr/local/bin/vim
+sudo update-alternatives --install /usr/bin/vi vi /usr/local/bin/vim 1
+sudo update-alternatives --set vi /usr/local/bin/vim
