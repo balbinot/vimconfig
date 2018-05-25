1db1b5fe3c7622611f2ba59f555e7ee119ddb856
Eduardo Balbinot <e.balbinot@surrey.ac.uk>
2018-05-25 23:57:53 +0100

    compile options for vi

---
+++ compile_options.sh
@@ -0,0 +1,12 @@
+./configure --with-features=huge \
+							--enable-multibyte \
+							--enable-rubyinterp=yes \
+							--enable-pythoninterp=yes \
+							--with-python-config-dir=/home/eb0025/anaconda2/lib/python2.7/config \
+							--enable-python3interp=yes \
+							--with-python3-config-dir=/usr/lib/python3.5/config \
+							--enable-perlinterp=yes \
+							--enable-luainterp=yes \
+							--enable-gui=gtk2 \
+							--enable-cscope \
+							--prefix=/usr/local
